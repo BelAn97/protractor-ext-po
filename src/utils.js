1b1faa9fbf7371c21e426eaccd883df21f0b756c
Andrei Belousov <Andrei_belousov@epam.com>
2019-09-05 20:04:00 +0300

    ###1.2.0 new features

--- src/utils.js
+++ src/utils.js
@@ -4,67 +4,67 @@
 (() => {
     Object.assign(Array.prototype, {
 
-            contains(it) {
-                return this.indexOf(it) > -1;
-            },
-
-            containsOneOf(arr) {
-                var found = false;
-                this.forEach((it) => {
-                    if (arr.indexOf(it) > -1) {
-                        found = true;
-                    }
-                });
-                return found;
-            },
-
-            toLowerCase() {
-                return this.map((el) => {
-                    return el.toLowerCase();
-                });
-            },
-
-            toUpperCase() {
-                return this.map((el) => {
-                    return el.toUpperCase();
-                });
-            },
-
-            removeEmpty() {
-                return this.filter((el) => {
-                    return !el.isEmpty();
-                });
-            }
+        contains(it) {
+            return this.indexOf(it) > -1;
         },
 
-        Object.assign(String.prototype, {
+        containsOneOf(arr) {
+            var found = false;
+            this.forEach((it) => {
+                if (arr.indexOf(it) > -1) {
+                    found = true;
+                }
+            });
+            return found;
+        },
 
-            contains(it) {
-                return this.indexOf(it) !== -1;
-            },
+        toLowerCase() {
+            return this.map((el) => {
+                return el.toLowerCase();
+            });
+        },
 
-            containsOneOf(arr) {
-                var found = false;
-                var text = this;
-                arr.forEach((it) => {
-                    if (text.contains(it)) {
-                        found = true;
-                    }
-                });
-                return found;
-            },
+        toUpperCase() {
+            return this.map((el) => {
+                return el.toUpperCase();
+            });
+        },
 
-            format() {
-                var args = arguments;
-                var i = -1;
-                return this.replace(/\{(?:[^{}]|\{*\})*\}/g, (val) => {
-                    i++;
-                    return args[i] !== undefined ? args[i] : val;
-                });
-            },
+        removeEmpty() {
+            return this.filter((el) => {
+                return !el.isEmpty();
+            });
+        }
+    });
 
-            getDigits() {
-                return this.match(/\d+/)[0]
-            }
-        }))
+    Object.assign(String.prototype, {
+
+        contains(it) {
+            return this.indexOf(it) !== -1;
+        },
+
+        containsOneOf(arr) {
+            var found = false;
+            var text = this;
+            arr.forEach((it) => {
+                if (text.contains(it)) {
+                    found = true;
+                }
+            });
+            return found;
+        },
+
+        format() {
+            var args = arguments;
+            var i = -1;
+            return this.replace(/\{(?:[^{}]|\{*\})*\}/g, (val) => {
+                i++;
+                return args[i] !== undefined ? args[i] : val;
+            });
+        },
+
+        getDigits() {
+            return this.match(/\d+/)[0]
+        }
+    })
 })();
